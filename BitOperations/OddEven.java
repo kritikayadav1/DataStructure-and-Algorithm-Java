4464f5d2027c9c16e0378fe2bf42954d696cf868
Kritika Yadav <kritikayadav125@gmail.com>
2026-07-26 12:43:42 +0530

    BIT OPERATIONS

---
+++ BitOperations/OddEven.java
@@ -0,0 +1,13 @@
+package BitOperations;
+
+public class OddEven {
+    public static void main(String args[]) {
+        int n = 10;
+        int bitMask = 1;
+        if((n & bitMask) == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
