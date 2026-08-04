6679aa7d66767721c0aa40f74238f4bdd1ccfece
Kritika Yadav <kritikayadav125@gmail.com>
2026-08-04 21:23:22 +0530

    Added recursion power and tiling problem

--- RecursionPart2/TilingProblem.java
+++ RecursionPart2/TilingProblem.java
@@ -1,13 +1,14 @@
 package RecursionPart2;
 
+
 public class TilingProblem {
     public static int tilingProblem(int n){
         if(n==0||n==1){
             return 1;
         }
-        int verticalWays= tilingProblem(n-1);
-        int horizontalWays=tilingProblem(n-2);
-        return verticalWays+horizontalWays;
+        int a= tilingProblem(n-1);
+        int b=tilingProblem(n-2);
+        return a+b;
     }
     public static void main(String[] args){
 
