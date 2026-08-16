179ae376fb3837d1bc58afa306a0e6accf565550
Kritika Yadav <kritikayadav125@gmail.com>
2026-08-16 20:31:34 +0530

    Majority  question

--- DividenConquer/Majority.java
+++ DividenConquer/Majority.java
@@ -1,5 +1,5 @@
 package DividenConquer;
-public class MajorityElement {
+public class Majority {
 
     static int majorityElement(int[] nums) {
 
@@ -28,7 +28,7 @@ public class MajorityElement {
 
     public static void main(String[] args) {
 
-        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        int[] nums = {2, 2, 1, 1, 2, 1, 3};
 
         System.out.println(majorityElement(nums));
     }
