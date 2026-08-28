1bb83b409f5031641518ce669d7daade8df25a3a
Kritika Yadav <kritikayadav125@gmail.com>
2026-08-28 23:40:07 +0530

    KeypadCombination Ques

--- Backtracking/KeypadCombination.java
+++ Backtracking/KeypadCombination.java
@@ -1,44 +1,62 @@
-import java.util.*;
+package Backtracking;
+public class KeypadCombination {
 
-class Solution {
-
-    String[] mapping = {
-        "", "", "abc", "def", "ghi",
-        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    // Har digit ke corresponding letters
+    final static char[][] L = {
+        {},                 // 0
+        {},                 // 1
+        {'a', 'b', 'c'},    // 2
+        {'d', 'e', 'f'},    // 3
+        {'g', 'h', 'i'},    // 4
+        {'j', 'k', 'l'},    // 5
+        {'m', 'n', 'o'},    // 6
+        {'p', 'q', 'r', 's'}, // 7
+        {'t', 'u', 'v'},    // 8
+        {'w', 'x', 'y', 'z'}  // 9
     };
 
-    List<String> ans = new ArrayList<>();
+    public static void letterCombinations(String D) {
 
-    public List<String> letterCombinations(String digits) {
+        int len = D.length();
 
-        if (digits.length() == 0) {
-            return ans;
-        }
-
-        backtrack(digits, 0, "");
-
-        return ans;
-    }
-
-    void backtrack(String digits, int index, String current) {
-
-        // Agar saare digits process ho gaye
-        if (index == digits.length()) {
-            ans.add(current);
+        // Agar input empty hai
+        if (len == 0) {
+            System.out.println("");
             return;
         }
 
-        // Current digit
-        int digit = digits.charAt(index) - '0';
+        bfs(0, len, new StringBuilder(), D);
+    }
 
-        // Us digit ke letters
-        String letters = mapping[digit];
+    public static void bfs(
+            int pos,
+            int len,
+            StringBuilder sb,
+            String D) {
 
-        // Har letter try karo
-        for (char ch : letters.toCharArray()) {
+        // Saare digits process ho gaye
+        if (pos == len) {
+            System.out.println(sb.toString());
+        } else {
 
-            // Letter add
-            backtrack(digits, index + 1, current + ch);
+            // Current digit ke letters nikalo
+            char[] letters =
+                L[Character.getNumericValue(D.charAt(pos))];
+
+            // Har letter ko try karo
+            for (int i = 0; i < letters.length; i++) {
+
+                bfs(
+                    pos + 1,
+                    len,
+                    new StringBuilder(sb).append(letters[i]),
+                    D
+                );
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        letterCombinations("2");
     }
 }
